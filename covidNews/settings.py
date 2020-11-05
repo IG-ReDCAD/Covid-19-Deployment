0821939e93d7403d06b2752094efc16043fe61ad
IG-ReDCAD <graja.imen@gmail.com>
2020-11-05 06:22:30 +0000

    Updated files

--- covidNews/settings.py
+++ covidNews/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*eaei56p=3k!ubkb(wz-q-sp4ia-@%z(a!h^%vm*lffx2v6s#!'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sentimentcovid19news.herokuapp.com']
+# ALLOWED_HOSTS = []
 
 
 # Application definition
--- searchNews/documents.py
+++ searchNews/documents.py
@@ -13,31 +13,38 @@ ip = os.environ.get('IP')
 es = Elasticsearch(['http://' + ip])
 
 def display_news(es):
+    try:
 
-    query = {'size': 1000, 'sort' : [{'publishedAt' : {'order' : 'desc'}}]}
-    
-    data = es.search(index="news-articles", body=query)
-    articles = data['hits']['hits']
-   
-    article_list = []
-    list_words = ['covid-19', 'covid19' 'virus', 'coronavirus', \
-    'pandemic', 'sars', 'sars-cov-2', 'endemic', 'epidemic', 'quarantine', \
-    'vaccine', 'asymptomatic', 'incubation', 'spread', 'containment',\
-     'pneumonia', 'disease']
+        query = {'size': 1000, 'sort' : [{'publishedAt' : {'order' : 'desc'}}]}
+        
+        data = es.search(index="news-articles", body=query)
+        articles = data['hits']['hits']
+       
+        article_list = []
+        list_words = ['covid-19', 'covid19' 'virus', 'coronavirus', \
+        'pandemic', 'sars', 'sars-cov-2', 'endemic', 'epidemic', 'quarantine', \
+        'vaccine', 'asymptomatic', 'incubation', 'spread', 'containment',\
+         'pneumonia', 'disease']
 
-    for article in articles:
-        new_article = article['_source']
-        
-        if any(word in (new_article['title']).lower() for word in list_words):
-            article_dict = {'source': new_article['source_name'],
-                            'title': new_article['title'],
-                            'description': new_article['description'],
-                            'url': new_article['url'],
-                            'publication_date': new_article['publishedAt'][:10]}
-                  
-            article_list.append(article_dict)
-        
-    return article_list[:17]
+        for article in articles:
+            new_article = article['_source']
+            
+            if any(word in (new_article['title']).lower() for word in list_words):
+                article_dict = {'source': new_article['source_name'],
+                                'title': new_article['title'],
+                                'description': new_article['description'],
+                                'url': new_article['url'],
+                                'publication_date': new_article['publishedAt'][:10]}
+                      
+                article_list.append(article_dict)
+            
+        return article_list[:17]
+    except:
+        return {'source': "imen",
+                'title': "imen",
+                'description': "imen",
+                'url': "imen",
+                'publication_date': "20-20-2020"}
 
 def daily_sent_analysis():
 
