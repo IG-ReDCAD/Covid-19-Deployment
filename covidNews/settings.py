e3d851fe9f3568dbcdf58ca0053915c8530eed57
IG-ReDCAD <graja.imen@gmail.com>
2020-11-05 07:04:26 +0000

    added django_heroku

--- covidNews/settings.py
+++ covidNews/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,6 +125,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
 
 CRONJOBS = [('*/10 * * * *','searchNews.cron.call_api', '>>/tmp/scheduled_job.log 2>&1')]
 
